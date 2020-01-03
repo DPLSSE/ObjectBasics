e861539d7726182815f8d8439180382298879e24
Doug Durham <ddurham@dontpaniclabs.com>
2020-01-02 20:41:54 -0600

    Created soluttion and GarageDoor class

---
+++ ObjectBasics/ObjectBasics/GarageDoor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectBasics
+{
+    public class GarageDoor
+    {
+        string _doorId = "";
+        public bool Open = false;
+
+        public GarageDoor(string doorId)
+        {
+            _doorId = doorId;
+        }
+
+        public void ChangeDoor()
+        {
+            if (!Open)
+            {
+                // open the door and wait until it is open
+                // ...
+                SendToggleSignal();
+
+                // Set the door open status
+                Open = CheckIfDoorOpen();
+            }
+        }
+
+        void SendToggleSignal()
+        {
+            // send signal to the garage door system to change the door status
+            // ...
+        }
+
+        bool CheckIfDoorOpen()
+        {
+            bool doorOpen = false;
+            // send signal to the garage door system to determine if door is open
+            // ...
+            return doorOpen;
+        }
+    }
+}
